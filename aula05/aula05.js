97c92b512c7f83e473837790cf2dc13a5dd79891
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2022-05-10 21:05:04 -0300

    finalização do exercicio

--- aula05/aula05.js
+++ aula05/aula05.js
@@ -2,3 +2,13 @@ function soma(n1, n2) {
   return n1 + n2;
 }
 console.log(soma(1, 3));
+
+function repeat(inf, qtde) {
+  return inf.repeat(qtde);
+}
+console.log(repeat('oi', 5));
+
+const funcaoAnonima = (inf, qtde) => {
+  return inf.repeat(qtde);
+};
+console.log(funcaoAnonima);
---
+++ aula05/exercicio05.js
@@ -0,0 +1,38 @@
+// questão 01
+function teste01(x, y) {
+  return y - x;
+}
+console.log(teste01(100, 50));
+
+//questao2
+function polegadaParaCentimetro(polegada) {
+  return `${polegada} polegadas equivalem a ${polegada * 2.4} centímetros`;
+}
+console.log(polegadaParaCentimetro(10));
+
+//questao 3
+function converteParaUrl(string) {
+  return `http://www.${string}.com.br`;
+}
+console.log(converteParaUrl('google'));
+
+//questão 4
+function idadeDoCachorro(anos, nome_do_cachorro) {
+  return `${nome_do_cachorro} tem ${anos} anos, isso equivale a ${
+    anos * 7
+  } anos.`;
+}
+console.log(idadeDoCachorro(7, 'Lessy'));
+
+//questao 5
+function valorDaHora(salario) {
+  return `O salário R$ ${salario} é equivalente à ${salario / 160} horas.`;
+}
+console.log(valorDaHora(3500));
+
+//questao 6
+function imc(altura, peso) {
+  const imc = peso / (altura * altura);
+  return `Seu imc equivale a: ${imc}`;
+}
+console.log(imc(1.77, 90));
