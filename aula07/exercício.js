0ca6220d8c1f581af0219be42ba6212694fe16f2
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2022-05-16 20:41:30 -0300

    ...

---
+++ aula07/exercício.js
@@ -0,0 +1,34 @@
+//1
+console.log(!true);
+
+//2
+console.log(!false);
+
+//3
+console.log(!!false);
+
+//4
+console.log(true);
+
+//5
+console.log(!1);
+
+//6
+console.log(!!1);
+
+//7
+console.log(!0);
+
+//8
+console.log(!!0);
+
+//9
+console.log(!!'');
+
+//10
+let x = 5;
+let y = 9;
+
+//a
+const expressao = x < 10 && x !== 5;
+console.log(expressao, 'expressão');
